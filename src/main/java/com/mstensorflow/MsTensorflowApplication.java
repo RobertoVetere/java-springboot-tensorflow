6e4b5c15cee9ca1cf63230644e3f4682c6b3a5e9
Roberto Vetere <Roberto.vetere@gmail.com>
2023-11-14 18:39:16 +0100

    first commit

---
+++ src/main/java/com/mstensorflow/MsTensorflowApplication.java
@@ -0,0 +1,13 @@
+package com.mstensorflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTensorflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTensorflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mstensorflow/MsTensorflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstensorflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTensorflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
