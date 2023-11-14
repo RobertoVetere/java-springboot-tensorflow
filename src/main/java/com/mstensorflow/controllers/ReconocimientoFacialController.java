4713a9f4c318f8e266ac2666bc3097cfa8b5f19c
Roberto Vetere <Roberto.vetere@gmail.com>
2023-11-14 21:00:34 +0100

    ADD ReconocimientoFacialService

--- src/main/java/com/mstensorflow/controllers/ReconocimientoFacialController.java
+++ src/main/java/com/mstensorflow/controllers/ReconocimientoFacialController.java
@@ -1,4 +1,9 @@
 package com.mstensorflow.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reconocimiento-facial")
 public class ReconocimientoFacialController {
 }
